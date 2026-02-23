aa84f30efbc9b52563b60b600570d30836fa66f4
Abhishek Jha <abhibj2003@gmail.com>
2026-02-23 12:49:06 +0530

    removed duplicate path require

--- Backend/server.js
+++ Backend/server.js
@@ -4,12 +4,6 @@ const dotenv = require("dotenv");
 const path = require("path");
 const fs = require("fs");
 
-const uploadDir = path.join(__dirname, "uploads");
-
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
-
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const employeeRoutes = require("./routes/employeeRoutes");
