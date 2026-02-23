79c659ff1baa848beb7d0ba5e060a02cf1ec8abb
Abhishek Jha <abhibj2003@gmail.com>
2026-02-23 12:41:54 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
+const fs = require("fs");
+const path = require("path");
+
+const uploadDir = path.join(__dirname, "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
